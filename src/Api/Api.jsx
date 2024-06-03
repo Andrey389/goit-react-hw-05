0724c6ad852aa23097fa6fb261eee1f521f9ee50
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-06-03 22:35:40 +0300

    add Api

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,26 +1,35 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/';
-
-const options = {
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWVlNjYxNWE0YzViNWUxNmNiYzdkNzc5ZWI3ZDEzNSIsInN1YiI6IjY2NWM5ZjM1YjQyZDQ4NDc4OGNjNTE1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._khN5RgT8x_KTMW1BArPj0IwHNiPXA0mM1sp4pZU2jY',
-  },
-  params: {
-    page: 1,
-    include_adult: false,
-    language: 'en-US',
-  },
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.headers.common.Authorization =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWVlNjYxNWE0YzViNWUxNmNiYzdkNzc5ZWI3ZDEzNSIsInN1YiI6IjY2NWM5ZjM1YjQyZDQ4NDc4OGNjNTE1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._khN5RgT8x_KTMW1BArPj0IwHNiPXA0mM1sp4pZU2jY';
+axios.defaults.params = {
+  language: 'en-US',
 };
 
-export async function getApi() {
-  const response = await axios.get(`3/trending/movie/day`, options);
-  return response.data;
-}
+export const getTrendingmovies = async () => {
+  const response = await axios.get('/trending/movie/day');
+  return response.data.results;
+};
 
-export async function getMovieDetails(movieId) {
-  const response = await axios.get(`/3/movie/${movieId}`, options);
+export const getSearchmovie = async query => {
+  const response = await axios.get('/search/movie', {
+    params: { query: query },
+  });
+  return response.data.results;
+};
+
+export const getMoviedetails = async id => {
+  const response = await axios.get(`/movie/${id}`);
   return response.data;
-}
+};
+
+export const getMoviecredits = async id => {
+  const response = await axios.get(`/movie/${id}/credits`);
+  return response.data;
+};
+
+export const getMoviereviews = async id => {
+  const response = await axios.get(`/movie/${id}/reviews`);
+  return response.data.results;
+};
--- src/Pages/HomePages/HomePages.jsx
+++ src/Pages/HomePages/HomePages.jsx
@@ -1,33 +1,26 @@
 import { useEffect, useState } from 'react';
-import { getApi } from '../../Api/Api';
-import MovieList from '../../Components/MovieList/MovieList';
+import {
+  getTrendingmovies,
+  getSearchmovie,
+  getMoviedetails,
+  getMoviecredits,
+  getMoviereviews,
+} from '../../Api/Api';
+// import MovieList from '../../Components/MovieList/MovieList';
 
 export default function HomePages() {
-  const [movies, setMovies] = useState([]);
-  //   const [isLoader, setIsLoader] = useState(false);
-  //   const [isError, setIsError] = useState(false);
-
   useEffect(() => {
-    async function fetchMovies() {
-      try {
-        // setIsLoader(true);
-        // setIsError(false);
-        const data = await getApi();
-        setMovies(data.results);
-      } catch (error) {
-        console.log(error);
-        // setIsError(true);
-      } finally {
-        // setIsLoader(false);
-      }
-    }
-    fetchMovies();
+    getTrendingmovies();
+    getSearchmovie('cars');
+    getMoviedetails('95');
+    getMoviecredits('22');
+    getMoviereviews('1');
   }, []);
   return (
     <div>
       <h1>Trending today !</h1>
       {/* {isLoader && <Loader />} */}
-      {movies.length > 0 && <MovieList movies={movies} />}
+      {/* {movies.length > 0 && <MovieList movies={movies} />} */}
       {/* {isError && <ErrorMessage />} */}
     </div>
   );
